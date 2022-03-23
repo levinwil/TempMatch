ade18959ad4c865e87718ba0bb8e5b2c223221b4
Will LeVine <levinewill@icloud.com>
2022-03-23 15:06:00 -0400

    seed 6

--- benchmarks/svhn_4_labels_per_class.sh
+++ benchmarks/svhn_4_labels_per_class.sh
@@ -1,1 +1,1 @@
-python ../tempmatch/train.py --dataset svhn --num-labeled 40 --arch wideresnet --batch-size 64 --lr 0.03 --expand-labels --seed 5 --out model_checkpoints/svhn@40
+python ../tempmatch/train.py --dataset svhn --num-labeled 40 --arch wideresnet --batch-size 64 --lr 0.03 --expand-labels --seed 6 --out model_checkpoints/svhn@40
